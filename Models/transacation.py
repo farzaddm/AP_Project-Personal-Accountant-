f9fcd5bd2d478949551a558e02080140caf4aa36
farzaddm <farzad.d.m84@gmail.com>
2024-05-31 22:12:58 +0330

    change search

--- Models/transacation.py
+++ Models/transacation.py
@@ -1,17 +1,17 @@
 from Database.database import database
+from Utils.save import ISave
 
-from save import ISave
-class Transacation(Save):
-    def __init__(self,username,type,price,date,source_of_price,description,type_of_price):
+
+class Transacation(ISave):
+    def __init__(self, username, type, price, date, source_of_price, description, type_of_price):
         super().__init__()
-        self.username:str=username
-        self.type:str=type
-        self.price:str=price
-        self.date:str=date
-        self.source_of_price:str=source_of_price
-        self.description:str=description
-        self.type_of_price:str=type_of_price
-
+        self.username: str = username
+        self.type: str = type
+        self.price: str = price
+        self.date: str = date
+        self.source_of_price: str = source_of_price
+        self.description: str = description
+        self.type_of_price: str = type_of_price
 
         self.db = database()
 
